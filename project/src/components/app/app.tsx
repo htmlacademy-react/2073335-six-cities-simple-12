821b7dcf1c615df30723b1229470f2196577b750
Alexandra <dom.levchenko.l7@gmail.com>
2023-02-28 22:37:59 +0500

    2.16. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,14 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+
+import WelcomeScreen from '../../pages/main-page/main-page';
+
+type AppScreenProps = {
+  rentOffers: number;
+}
+
+function App({rentOffers}: AppScreenProps): JSX.Element {
+  return (
+    <WelcomeScreen rentOffers={rentOffers}/>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  RentOffers: 444,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      rentOffers = {Setting.RentOffers}
+    />
   </React.StrictMode>,
 );
