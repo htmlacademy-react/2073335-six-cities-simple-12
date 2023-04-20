7a3a2518231bc868e9851f85ed0813f14c34d3c6
Alexandra <dom.levchenko.l7@gmail.com>
2023-04-20 18:15:22 +0500

    На защиту

--- project/src/constants/const.ts
+++ project/src/constants/const.ts
@@ -1,4 +1,3 @@
-import { Offer } from '../types/offer';
 
 export enum AppRoute {
     Login = '/login',
@@ -30,45 +29,6 @@ export const OFFERS_SORT_OPTIONS = [
   'Price: high to low',
   'Top rated first',
 ];
-
-export const UNKNOWN_OFFER:Offer = {
-  bedrooms: 0,
-  city: {
-    location: {
-      latitude: 0,
-      longitude: 0,
-      zoom: 0
-    },
-    name: ''
-  },
-  description: '',
-  goods: [
-    ''
-  ],
-  host: {
-    avatarUrl: '',
-    id: 0,
-    isPro: false,
-    name: ''
-  },
-  id: 0,
-  images: [
-    ''
-  ],
-  isFavorite: false,
-  isPremium: false,
-  location: {
-    latitude: 0,
-    longitude:0,
-    zoom: 0
-  },
-  maxAdults: 0,
-  previewImage: '',
-  price: 0,
-  rating: 0,
-  title: '',
-  type: ''
-};
 
 export const MAX_COUNT_REVIEWS = 10;
 export const MIN_TEXT_COMMENT = 50;
